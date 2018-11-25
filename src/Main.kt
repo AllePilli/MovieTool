0b3e3ed32be23c88a66ee51a3d366d92037fe8ff
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-11-25 15:50:33 +0100

    Added command line function "ren"

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>){
-    Renamer.start(args[0], args[1], args[2])
+    val fnc = args[0]
+
+    when (fnc){
+        "ren" -> Renamer.start(args[1], args[2], args[3]) //Rename files in directory: ren path epPrefix newName
+    }
 }
