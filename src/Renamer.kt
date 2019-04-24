eb49184faa9c4bb2bbc45d44575e114627e22833
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2019-04-24 10:48:59 +0200

    Dont rename files with correct filenames

--- src/Renamer.kt
+++ src/Renamer.kt
@@ -4,12 +4,16 @@ import java.io.File
 class Renamer{
     companion object {//TODO: multiple files renamed at the same time
         private val SE_REGEX = "[sS]\\d{2}[eE]\\d{2}|[1-9][xX]\\d{2}".toRegex()
+        private val CORRECT_REGEX = "^\\d{1,2}x\\d{2}-\\w+\\.\\w+$".toRegex()
 
         fun start(path: String){
             val dir = File(path)
             val done = hashMapOf<String, String>()
 
-            dir.walkTopDown().filter { !it.isDirectory }.forEach { file ->
+            dir.walkTopDown()
+                .filter { !it.isDirectory }                         // Dont rename directories
+                .filter { !CORRECT_REGEX.matches(it.pureName()) }   // Dont rename files with correct filenames
+                .forEach { file ->
                 val match = SE_REGEX.find(file.pureName())
                 val epPrefix = if (match != null){
                     val matchedVal = match.value
